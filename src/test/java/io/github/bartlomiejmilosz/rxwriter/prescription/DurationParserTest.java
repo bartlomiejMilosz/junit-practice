06ba3f918a7d2937f85f29d77806e75f5613819f
Bart Milo <bartlomiej.milosz@gmail.com>
2024-01-30 20:12:23 +0100

    Add junit-jupiter-params dependency and create parametrized test for parseDuration() in all cases

--- src/test/java/io/github/bartlomiejmilosz/rxwriter/prescription/DurationParserTest.java
+++ src/test/java/io/github/bartlomiejmilosz/rxwriter/prescription/DurationParserTest.java
@@ -1,13 +1,29 @@
 package io.github.bartlomiejmilosz.rxwriter.prescription;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class DurationParserTest {
+    @ParameterizedTest
+    @CsvSource({
+            "day,       1",
+            "3 days,    3",
+            "week,      7",
+            "2 weeks,   14",
+            "month,     30",
+            "6 months,  180",
+            "months,    0"
+    })
+    void parseDurationWithValidUnitAnsQuantity(String duration, int expectedNumberOfDays) {
+        Assertions.assertEquals(expectedNumberOfDays, DurationParser.parseDays(duration));
+    }
 
     @Test
-    void parseDurationWithValidUnitAnsQuantity() {
-        assertEquals(14, DurationParser.parseDays("2 weeks"));
+    void returnsNullForUnmatchedUnit() {
+        assertNull(DurationUnit.getByTextValue("boop"));
     }
 }
