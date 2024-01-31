9edfe97eee7b1fa459aa5375c1744c6324e59693
Bart Milo <bartlomiej.milosz@gmail.com>
2024-01-31 20:04:37 +0100

    Add parametrized test method for case of opiates concept

--- src/test/java/io/github/bartlomiejmilosz/rxwriter/drug/DrugConceptTest.java
+++ src/test/java/io/github/bartlomiejmilosz/rxwriter/drug/DrugConceptTest.java
@@ -1,23 +1,69 @@
 package io.github.bartlomiejmilosz.rxwriter.drug;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DrugConceptTest {
 
     public final static DrugConcept TEST_CONCEPT_OPIATES = new DrugConcept(
-            new DrugClassification[] {
+            new DrugClassification[]{
                     DrugClassification.ANTIANXIETY,
                     DrugClassification.ANALGESICS_NARCOTIC,
                     DrugClassification.NARCOTIC_ANTHISTAMINE
             });
 
+    static Stream<Arguments> drugDataProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new DispensableDrug(
+                                "test-drug-1",
+                                new DrugClassification[]{
+                                        DrugClassification.ANTIANXIETY,
+                                        DrugClassification.ANTIBACTERIAL
+                                },
+                                false),
+                        true // Assuming the drug is expected to match the concept
+                ),
+                Arguments.of(
+                        new DispensableDrug(
+                                "test-drug-2",
+                                new DrugClassification[]{
+                                        DrugClassification.ANTIFUNGAL,
+                                        DrugClassification.ANTIBACTERIAL},
+                                false),
+                        false // Assuming the drug is not expected to match the concept
+                ),
+                Arguments.of(
+                        new DispensableDrug(
+                                "test-drug-3",
+                                new DrugClassification[]{
+                                        DrugClassification.ANALGESIC,
+                                        DrugClassification.NARCOTIC_ANTHISTAMINE},
+                                false),
+                        true // Assuming the drug is expected to match the concept
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("drugDataProvider")
+    void drugBelongsInConceptIfOneClassMatches(DispensableDrug drug, boolean expected) {
+        final DrugConcept testConcept = DrugConcept.OPIATES; // Assuming this is tested concept
+        assertEquals(expected, testConcept.isDrugInConcept(drug));
+    }
+
     @Test
     void drugBelongsInConceptIfOneClassMatches() {
         DispensableDrug drug = new DispensableDrug(
                 "test-drug",
-                new DrugClassification[] {
+                new DrugClassification[]{
                         DrugClassification.ANTIANXIETY,
                         DrugClassification.ANTIBACTERIAL
                 },
